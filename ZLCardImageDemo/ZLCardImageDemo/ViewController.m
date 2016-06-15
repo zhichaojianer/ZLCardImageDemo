e2f48778fd2da2bc5c72e0750774f5bff4646e3d
liuchao <liuchao@imzhiliao.com>
2016-06-15 09:43:07 +0800

    集成ZLSwipeableView

--- ZLCardImageDemo/ZLCardImageDemo/ViewController.m
+++ ZLCardImageDemo/ZLCardImageDemo/ViewController.m
@@ -8,8 +8,17 @@
 
 #import "ViewController.h"
 #import "ZLCardScrollView.h"
+#import "ZLSwipeableView.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
-@interface ViewController ()
+@interface ViewController ()<ZLSwipeableViewDataSource, ZLSwipeableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet ZLSwipeableView *swipeableView;
+
+@property (nonatomic, strong) NSMutableArray *swipeableArray;
+
+@property (nonatomic) NSUInteger flagIndex;
+@property (nonatomic) NSUInteger swipeableIndex;
 
 @end
 
@@ -35,11 +44,74 @@
     [zlCardImages addObject:@"http://pic1.nipic.com/2008-12-09/200812910493588_2.jpg"];
     [zlCardImages addObject:@"http://img.61gequ.com/allimg/2011-4/201142614314278502.jpg"];
     [zlCardScrollView setZlCardImageArray:zlCardImages];
+    
+    
+    [self.swipeableView setNeedsLayout];
+    [self.swipeableView layoutIfNeeded];
+    
+    self.swipeableView.dataSource = self;
+    self.swipeableView.delegate = self;
+    
+    self.flagIndex = 0;
+    self.swipeableIndex = 0;
+    self.swipeableArray = [NSMutableArray arrayWithArray:zlCardImages];
+    [self.swipeableView discardAllSwipeableViews];
+    [self.swipeableView loadNextSwipeableViewsIfNeeded];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark -
+#pragma mark ZLSwipeableViewDelegate
+
+- (void)swipeableView: (ZLSwipeableView *)swipeableView didSwipeLeft:(UIView *)view {
+    self.flagIndex--;
+    if (self.flagIndex < 1) {
+        self.swipeableIndex = 0;
+        [self.swipeableView discardAllSwipeableViews];
+        [self.swipeableView loadNextSwipeableViewsIfNeeded];
+    }
+}
+- (void)swipeableView: (ZLSwipeableView *)swipeableView didSwipeRight:(UIView *)view {
+    self.flagIndex--;
+    if (self.flagIndex < 1 ) {
+        self.swipeableIndex = 0;
+        [self.swipeableView discardAllSwipeableViews];
+        [self.swipeableView loadNextSwipeableViewsIfNeeded];
+    }
+}
+
+#pragma mark -
+#pragma mark ZLSwipeableViewDataSource
+
+- (UIView *)nextViewForSwipeableView:(ZLSwipeableView *)swipeableView {
+    if (self.swipeableIndex < self.swipeableArray.count) {
+        CGFloat width = [UIScreen mainScreen].bounds.size.width;
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, width-16-20, (width-16)*3/4-20)];
+        
+        UIImageView *bgImageView = [[UIImageView alloc] initWithFrame:view.frame];
+        [bgImageView setImage:[UIImage imageNamed:@"baby_bg_image"]];
+        
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(8, 7, bgImageView.frame.size.width-16, bgImageView.frame.size.height-17)];
+        [imageView setBackgroundColor:[UIColor blackColor]];
+        NSURL *imageUrl = [NSURL URLWithString:self.swipeableArray[self.swipeableIndex]];
+        [imageView sd_setImageWithURL:imageUrl];
+//        [imageView setContentMode:UIViewContentModeScaleAspectFit];
+        
+        [bgImageView addSubview:imageView];
+        
+        [view addSubview:bgImageView];
+        
+        self.swipeableIndex++;
+        self.flagIndex++;
+        
+        return view;
+    }
+    return nil;
 }
 
 @end
